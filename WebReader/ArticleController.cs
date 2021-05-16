8544358253b0ac7dd87f970a6b903e372de73f46
Matjaž Miler <matjaz@miler.si>
2021-05-16 16:33:31 +0200

    WebReader controller

---
+++ WebReader/ArticleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebReader
+{
+	[Route("")]
+	public class ArticleController : Controller
+	{
+
+		[HttpGet("/{**articlePath}")]
+		public IActionResult ViewArticle(string articlePath)
+		{
+
+			Reader.Pages.ArticleViewModel model = new() { ArticlePath = articlePath };
+			return View("~/Areas/Reader/Pages/ArticleView.cshtml", model);
+
+			//return View();
+			return Content(articlePath);
+		}
+	}
+}
