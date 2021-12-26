2bb1ae6f552af2c2768b5ac45f77cce8f81ac773
Matjaž Miler <matjaz@miler.si>
2021-12-26 19:34:21 +0100

    Added Azure Key Vault to secrets store

---
+++ CommonCore/Secrets/AzureKeyVaultStore.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Docomb.CommonCore.Secrets
+{
+	public class AzureKeyVaultStore : Store
+	{
+		public AzureKeyVaultStore(string code, string vaultUri, Azure.Core.TokenCredential credential) : this(code, new Uri(vaultUri), credential)
+		{
+		}
+
+		public AzureKeyVaultStore(string code, Uri vaultUri, Azure.Core.TokenCredential credential)
+		{
+			_code = code;
+			_vaultUri = vaultUri;
+			_credential = credential;
+			_client = new(_vaultUri, _credential);
+		}
+
+		private string _code;
+		private Uri _vaultUri;
+		private Azure.Core.TokenCredential _credential;
+
+		private SecretClient _client;
+
+		public override string Code => _code;
+
+		public override string GetValue(string key) => GetValueAsync(key).Result;
+
+		public async Task<string> GetValueAsync(string key)
+		{
+			try
+			{
+				Azure.Response<KeyVaultSecret> secret = await _client.GetSecretAsync(key);
+				return secret.Value.Value;
+			}
+			catch (Exception e)
+			{
+				e.Report();
+				return null;
+			}
+		}
+
+	}
+}
